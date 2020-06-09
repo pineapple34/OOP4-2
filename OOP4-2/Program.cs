fbcb61b662b063ac9213e1941ed3880728097eaf
pineapple34 <pineapple6934@gmail.com>
2020-06-09 16:45:59 +0500

    Initial commit

---
+++ OOP4-2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Год - " + book.Year);
+                Console.WriteLine("Новое значение - ");
+                int.TryParse(Console.ReadLine(), out int newYear);
+                book.Year = newYear;
+            }
+        }
+    }
+
+    class Book
+    {
+        private int year;
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                if (value >= 1940 && value <= 2020) year = value;
+            }
+        }
+        public Book()
+        {
+            this.year = 1940;
+        }
+    }
+}
